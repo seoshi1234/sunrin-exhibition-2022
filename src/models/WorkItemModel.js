8fee3358b4f5e69104906179c7382c3aa0c5644a
정진오 <jino9492@gmail.com>
2022-05-08 03:09:32 +0900

    Work 완성 및 Detail 페이지 연결

---
+++ src/models/WorkItemModel.js
@@ -0,0 +1,8 @@
+export default class WorkItemModel{
+  constructor(imageURL, title, author, tag) {
+    this.imageURL = imageURL;
+    this.title = title;
+    this.author = author;
+    this.tag = tag;
+  }
+}
